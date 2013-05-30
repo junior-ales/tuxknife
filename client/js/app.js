140456ef07fce00dc43409c9730971e68abc1c90
Junior Ales <edilson.ales.jr@gmail.com>
2013-05-29 18:30:04 -0700

    Adding response to the user

--- client/js/app.js
+++ client/js/app.js
@@ -1,14 +1,19 @@
+var renderResponse = function(data) {
+  $('#response').append(JSON.stringify(data));
+}
+
 $('document').ready(function() {
   $('#submitButton').on('click', function() {
     var user = $('#username').val();
     var pass = $('#password').val();
     var server = $('#server').val();
+    var webservice = '10.71.2.89';
 
     $.ajax({
       type: "GET",
-      url: 'http://10.71.2.89:8083/servers/' + server,
+      url: 'http://' + webservice + ':8083/servers/' + server,
       data: { "username": user, "password": pass },
-      success: function(data) { console.log(data) },
+      success: function(data) { renderResponse(data) },
       error: function(data) { alert("faio"); console.log(data) },
       dataType: "json"
     });
