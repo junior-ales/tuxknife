b50cba33e35d4349ebb25bdcfa1c826d9ddb17e4
Junior Ales <edilson.ales.jr@gmail.com>
2013-12-15 00:53:13 -0200

    Expressjs configured

--- client/web/server.js
+++ client/web/server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/app/public'));
+
+app.set('views', __dirname + '/app/views');
+app.engine('html', require('ejs').renderFile);
+
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.render('index.html');
 });
 
 app.listen(3000);
